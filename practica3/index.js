53df9c623d3fed95fab71b10e54f03301fd3a261
maugarbru <mauriciogarzonb@gmail.com>
2020-04-03 16:30:58 -0500

    Cifrado de escitala

--- practica3/index.js
+++ practica3/index.js
@@ -21,12 +21,30 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + "/front/index.html"))
 })
 
-app.get('/escitala', (req, res) => {
+app.get('/escitala/descifrar', (req, res) => {
     var texto = req.query.text
     var llave = req.query.key
+    console.log("----------------------------");
+    console.log("... DESCIFRANDO ESCÍTALA ...");
+    console.log(`Texto: "${texto}" >> (Llave: ${llave})`);
     escitala.desencriptar(texto, llave).then(response => {
         res.status(200).send(response)
-        console.log(texto + " -->> " + response.texto);
+        console.log(`Texto descifrado ->> "${response.texto}"`);
+    }).catch(error => {
+        res.status(400).send(error)
+        console.log(error);
+    })
+})
+
+app.get('/escitala/cifrar', (req, res) => {
+    var texto = req.query.text
+    var llave = req.query.key
+    console.log("-------------------------");
+    console.log("... CIFRANDO ESCÍTALA ...");
+    console.log(`Texto: "${texto}" >> (Llave: ${llave})`);
+    escitala.encriptar(texto, llave).then(response => {
+        res.status(200).send(response)
+        console.log(`Texto cifrado ->> "${response.texto}"`);
     }).catch(error => {
         res.status(400).send(error)
         console.log(error);
--- practica3/scripts/escitala.js
+++ practica3/scripts/escitala.js
@@ -12,13 +12,13 @@ class Escitala {
         var cols = Math.ceil(
             texto.length / llave
         );
-        var cifrado = texto.split("");
+        var original = texto.split("");
         var matriz = [];
         for (let index = 0; index < rows; index++) {
             var rowTemporal = [];
             for (let index = 0; index < cols; index++) {
-                if (cifrado[contador]) {
-                    rowTemporal.push(cifrado[contador]);
+                if (original[contador]) {
+                    rowTemporal.push(original[contador]);
                 } else {
                     rowTemporal.push(" ");
                 }
@@ -26,16 +26,47 @@ class Escitala {
             }
             matriz.push(rowTemporal);
         }
-
-        var descifrado = ""
+        var resultado = ""
         for (let i = 0; i < cols; i++) {
             for (let j = 0; j < matriz.length; j++) {
-                descifrado += matriz[j][i]
+                resultado += matriz[j][i]
             }
         }
-
         var json = {
-            texto: descifrado.trim(),
+            texto: resultado,
+            matriz: matriz
+        }
+        return json
+    }
+
+    async encriptar(texto, llave) {
+        var contador = 0;
+        var rows = llave;
+        var cols = Math.ceil(
+            texto.length / llave
+        );
+        var original = texto.split("");
+        var matriz = [];
+        for (let index = 0; index < cols; index++) {
+            var rowTemporal = [];
+            for (let index = 0; index < rows; index++) {
+                if (original[contador]) {
+                    rowTemporal.push(original[contador]);
+                } else {
+                    rowTemporal.push(" ");
+                }
+                contador++;
+            }
+            matriz.push(rowTemporal);
+        }
+        var resultado = ""
+        for (let i = 0; i < rows; i++) {
+            for (let j = 0; j < matriz.length; j++) {
+                resultado += matriz[j][i]
+            }
+        }
+        var json = {
+            texto: resultado,
             matriz: matriz
         }
         return json
